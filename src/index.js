a413eb0fc5479fe78891c95991c291b9ae1108ae
Nadeem Anwar <57771677+nadeemds@users.noreply.github.com>
2021-12-10 10:56:34 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './component/App'
-import 'bootstrap/dist/css/bootstrap.css'
-
-//const element = <h1>hello world</h1>
-//in real world senario we render the whole app content instead of single element
-
-ReactDom.render(<App/>, document.getElementById("root"))
